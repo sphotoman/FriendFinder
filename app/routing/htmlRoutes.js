52a78ec51b488270a2531f50da73933641ef36c0
Scott Faber <scottfaberphoto@gmail.com>
2017-12-01 20:12:42 -0500

    initial commit... oops guess I should have done this sooner.

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,25 @@
+   // * A GET Route to `/survey` which should display the survey page.
+   
+   // * A default, catch-all route that leads to `home.html` which displays the home page. 
+// Dependencies
+// =============================================================
+var path = require("path");
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // Each of the below routes just handles the HTML page that the user gets sent to.
+
+  // index route loads view.html
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
+  // If no matching route is found default to home
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+};
